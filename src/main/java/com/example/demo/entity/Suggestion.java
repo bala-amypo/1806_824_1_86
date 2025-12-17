34a95874442961e421c82616736e795d4de37590
bala-amypo <bala@amypo.in>
2025-12-17 08:28:25 +0000

    User commit

--- src/main/java/com/example/demo/entity/Suggestion.java
+++ src/main/java/com/example/demo/entity/Suggestion.java
@@ -1,3 +1,30 @@
-public class Suggestion{
-    
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Suggestion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Farm farm;
+
+    private String suggestedCrops;
+    private String suggestedFertilizers;
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    void created() {
+        this.createdAt = LocalDateTime.now();
+    }
 }
