0709d90f7302d312d22c4d7ed02fd1e9b9534a12
bala-amypo <bala@amypo.in>
2025-12-21 10:11:22 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/FertilizerRequest.java
@@ -0,0 +1,23 @@
+package com.example.demo.dto;
+
+public class FertilizerRequest {
+
+    private String name;
+    private String cropName;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCropName() {
+        return cropName;
+    }
+
+    public void setCropName(String cropName) {
+        this.cropName = cropName;
+    }
+}
