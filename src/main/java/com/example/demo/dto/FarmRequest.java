694d0391941d9befa9efb6098fb3b097df60030a
bala-amypo <bala@amypo.in>
2025-12-26 17:34:39 +0000

    User commit

--- src/main/java/com/example/demo/dto/FarmRequest.java
+++ src/main/java/com/example/demo/dto/FarmRequest.java
@@ -1,5 +1,10 @@
 package com.example.demo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
 @AllArgsConstructor 
 @NoArgsConstructor 
 @Data
--- src/main/java/com/example/demo/dto/FertilizerRequest.java
+++ src/main/java/com/example/demo/dto/FertilizerRequest.java
@@ -1,4 +1,8 @@
 package com.example.demo.dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 
 @AllArgsConstructor @NoArgsConstructor @Data
 public class FertilizerRequest {
