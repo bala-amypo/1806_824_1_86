150f287b60ce196cab4f57f732f70c210f21c0fa
bala-amypo <bala@amypo.in>
2025-12-26 14:40:54 +0000

    User commit

--- src/main/java/com/example/demo/entity/Farm.java
+++ src/main/java/com/example/demo/entity/Farm.java
@@ -1,60 +1,24 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
-@Table(name = "farms")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Farm {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
-    private String farmName;
-
-    @Column(nullable = false)
     private String location;
+    private Double soilPh;
+    private String soilType;
 
-    private double size;
-
-    public Farm() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFarmName() {
-        return farmName;
-    }
-
-    public void setFarmName(String farmName) {
-        this.farmName = farmName;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public double getSize() {
-        return size;
-    }
-
-    public void setSize(double size) {
-        this.size = size;
-    }
+    @ManyToOne
+    private User owner;
 }
