fe62cb2a7eb4993c549dcbda8130cf1a4bbeac4a
bala-amypo <bala@amypo.in>
2025-12-26 18:16:40 +0000

    User commit

--- src/main/java/com/example/demo/repository/CropRepository.java
+++ src/main/java/com/example/demo/repository/CropRepository.java
@@ -5,9 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+@Repository
 public interface CropRepository extends JpaRepository<Crop, Long> {
 
-    List<Crop> findSuitableCrops(double ph, String season);
-
-    List<Crop> findSuitableCrops(double min, double max, String season);
+    @Query("SELECT c FROM Crop c WHERE c.nitrogen <= :nitrogen AND c.soilType = :soilType")
+    List<Crop> findSuitableCrops(@Param("nitrogen") double nitrogen,
+                                 @Param("soilType") String soilType);
 }
