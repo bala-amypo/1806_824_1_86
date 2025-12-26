b4d1671cd54d39a516faa0b1e81398694968d543
bala-amypo <bala@amypo.in>
2025-12-26 18:19:40 +0000

    User commit

--- src/main/java/com/example/demo/repository/CropRepository.java
+++ src/main/java/com/example/demo/repository/CropRepository.java
@@ -1,8 +1,10 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.Crop;
+import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 @Repository
