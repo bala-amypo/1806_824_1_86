b8de7cfbb012fadc71637830f1b20a0029cd4716
bala-amypo <bala@amypo.in>
2025-12-26 17:31:39 +0000

    User commit

--- src/main/java/com/example/demo/entity/Crop.java
+++ src/main/java/com/example/demo/entity/Crop.java
@@ -2,10 +2,13 @@ package com.example.demo.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Entity
 @Data
 @Builder
 @NoArgsConstructor
--- src/main/java/com/example/demo/entity/Fertilizer.java
+++ src/main/java/com/example/demo/entity/Fertilizer.java
@@ -1,6 +1,14 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 
 @Entity
 @Data
